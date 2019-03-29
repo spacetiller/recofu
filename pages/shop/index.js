b83207084facaa57c0871bae72fb40822e17427f
spacetiller <spacetiller@163.com>
2019-03-29 18:52:09 +0800

    update shop arch

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -6,19 +6,17 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-		title: "福分",
-		labels: ['行善积德', '福气好物'],
+		title: "福缘",
+		labels: ['福运', '福修'],
 		_num: 0,
 		goods: [
-			{pid: 1, name: '关注大凉山贫困儿童的生存现状', price: 100, ptype: 1, imgurl: 'http://image.pk4yo.com/recofu/goods_daliangshan_1.jpg', provider: '合作者'},
-			{pid: 2, name: '猫咪救助行动', price: 100, ptype: 2, imgurl: 'http://image.pk4yo.com/recofu/goods_cats_1.jpg', provider: '合作者'},
-			{pid: 3, name: '项目名称项目名称项目名称', price: 100, ptype: 1, imgurl: 'http://image.pk4yo.com/recofu/goods_hungry_1.jpg', provider: '合作者'},
-			{pid: 4, name: '项目名称项目名称项目名称', price: 100, ptype: 1, imgurl: 'http://image.pk4yo.com/recofu/goods_school_1.jpg', provider: '合作者'}
+			{ pid: 1, name: 'XXX居士 趋吉避凶妙招', price: 100, ptype: 1, imgurl: 'http://image.pk4yo.com/recofu/lucky_jushi_1.jpg', provider: '合作者'},
+			{ pid: 2, name: 'XXX居士 桃花运妙招', price: 100, ptype: 2, imgurl: 'http://image.pk4yo.com/recofu/lucky_taohua_1.jpg', provider: '合作者'},
+			{ pid: 3, name: 'XXX居士 旺财运妙招', price: 100, ptype: 1, imgurl: 'http://image.pk4yo.com/recofu/lucky_drum_1.jpg', provider: '合作者'}
 		], // 数据列表	list[]
 		products: [
-			{ pid: 1, name: '灭度沉香单圈佛珠手链男女款手串念珠平安吉祥礼物状', price: "6930.00", ptype: 3, imgurl: 'http://image.pk4yo.com/recofu/product_bracelet_1.jpg', provider: '智天乐游' },
-			{ pid: 2, name: '猫咪救助行动', price: "8193.50", ptype: 3, imgurl: 'http://image.pk4yo.com/recofu/product_rosary_1.jpg', provider: '智天乐游' },
-			{ pid: 3, name: '项目名称项目名称项目名称', price: "4098.50", ptype: 3, imgurl: 'http://image.pk4yo.com/recofu/product_bead_1.jpg', provider: '智天乐游' }
+			{ pid: 1, name: '灭度沉香单圈佛珠手链男女款手串念珠平安吉祥礼物状', price: 6930.00, priceStr: "6930.00", ptype: 3, imgurl: 'http://image.pk4yo.com/recofu/product_bracelet_1.jpg', provider: '智天乐游' },
+			{ pid: 3, name: '五台山天然野生老料六道木手串降龙木佛珠手链', price: "4098.50", ptype: 3, imgurl: 'http://image.pk4yo.com/recofu/product_bead_1.jpg', provider: '智天乐游' }
 		], // 数据列表	list[]
 		orderId: '', // 订单号
 		loading: false, // 显示等待框
